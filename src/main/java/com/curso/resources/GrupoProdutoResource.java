195a3fe8b1a439be00a50e9c450584df5c21e69b
jeffersonarpasserini <jefferson.passerini@gmail.com>
2025-04-24 14:27:57 -0300

    swagger implementado no projeto

--- src/main/java/com/curso/resources/GrupoProdutoResource.java
+++ src/main/java/com/curso/resources/GrupoProdutoResource.java
@@ -3,6 +3,8 @@ package com.curso.resources;
 import com.curso.domains.GrupoProduto;
 import com.curso.domains.dtos.GrupoProdutoDTO;
 import com.curso.services.GrupoProdutoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -16,23 +18,30 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping(value = "/grupoproduto")
+@Tag(name = "Grupo de Produtos", description="API para Gerenciamento de Grupo de Produtos")
 public class GrupoProdutoResource {
 
     @Autowired
     private GrupoProdutoService grupoProdutoService;
 
     @GetMapping //exemplo http://localhost:8080/produto
+    @Operation(summary = "Listar todos os grupos de produtos",
+            description = "Retorna uma lista com todos os grupos de produtos cadastrados")
     public ResponseEntity<List<GrupoProdutoDTO>> findAll(){
         return ResponseEntity.ok().body(grupoProdutoService.findAll());
     }
 
     @GetMapping(value = "/{id}") //exemplo http://localhost:8080/grupoproduto/1
+    @Operation(summary = "Busca um grupo de produto por id",
+            description = "Realiza a busca de um grupo de produto cadastrado por id")
     public ResponseEntity<GrupoProdutoDTO> findById(@PathVariable Integer id){
         GrupoProduto obj = this.grupoProdutoService.findbyId(id);
         return ResponseEntity.ok().body(new GrupoProdutoDTO(obj));
     }
 
     @PostMapping
+    @Operation(summary = "Criar um novo grupo de produto",
+            description = "Cria um novo grupo de produto com base nos dados fornecidos")
     public ResponseEntity<GrupoProdutoDTO> create(@Valid @RequestBody GrupoProdutoDTO dto) {
         GrupoProduto grupoProduto = grupoProdutoService.create(dto);
         // Cria o URI para o recurso criado
@@ -43,12 +52,16 @@ public class GrupoProdutoResource {
     }
 
     @PutMapping(value = "/{id}")
+    @Operation(summary = "Altera um grupo de produto",
+            description = "Altera um grupo de produto existente")
     public ResponseEntity<GrupoProdutoDTO> update(@PathVariable Integer id, @Valid @RequestBody GrupoProdutoDTO objDto){
         GrupoProduto Obj = grupoProdutoService.update(id, objDto);
         return ResponseEntity.ok().body(new GrupoProdutoDTO(Obj));
     }
 
     @DeleteMapping(value = "/{id}")
+    @Operation(summary = "Deletar um grupo de produto",
+            description = "Remove um grupo de produto a partir do seu Id")
     public ResponseEntity<GrupoProdutoDTO> delete(@PathVariable Integer id){
         grupoProdutoService.delete(id);
         return ResponseEntity.noContent().build();
--- src/main/java/com/curso/resources/ServiceOrderResource.java
+++ src/main/java/com/curso/resources/ServiceOrderResource.java
@@ -1,7 +1,10 @@
 package com.curso.resources;
 
+import com.curso.domains.ServiceOrder;
 import com.curso.domains.Technician;
+import com.curso.domains.dtos.ServiceOrderDTO;
 import com.curso.domains.dtos.TechnicianDTO;
+import com.curso.services.ServiceOrderService;
 import com.curso.services.TechnicianService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,8 +16,36 @@ import java.net.URI;
 import java.util.List;
 import java.util.UUID;
 
-
+@RestController
+@RequestMapping(value = "/serviceorder")
 public class ServiceOrderResource {
 
+    @Autowired
+    private ServiceOrderService osService;
 
+
+    @GetMapping(value = "/{id}") //exemplo http://localhost:8080/serviceorder/1
+    public ResponseEntity<ServiceOrderDTO> findById(@PathVariable UUID id){
+        ServiceOrder obj = this.osService.findbyId(id);
+        return ResponseEntity.ok().body(new ServiceOrderDTO(obj));
+    }
+
+    @GetMapping //exemplo http://localhost:8080/serviceorder
+    public ResponseEntity<List<ServiceOrderDTO>> findAll(){
+        return ResponseEntity.ok().body(osService.findAll());
+    }
+
+
+    @PostMapping
+    public ResponseEntity<ServiceOrderDTO> create(@Valid @RequestBody ServiceOrderDTO objDto){
+        ServiceOrder newObj = osService.create(objDto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newObj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ServiceOrderDTO> update(@PathVariable UUID id, @Valid @RequestBody ServiceOrderDTO objDto){
+        ServiceOrder Obj = osService.update(id, objDto);
+        return ResponseEntity.ok().body(new ServiceOrderDTO(Obj));
+    }
 }
